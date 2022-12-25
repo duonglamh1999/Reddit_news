c15e6507d187d6ad09f2bcadf422bf4b5c597d52
duonglamh1999 <duonglamh1999@gmail.com>
2022-12-25 15:30:12 -0600

    got dev key

--- project/script.py
+++ project/script.py
@@ -4,8 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 API_SECRET = os.getenv('API_SECRET')
-
+DEV_KEY = os.getenv('DEV_KEY')
 print(API_SECRET)
+print(DEV_KEY)
 # reddit = praw.Reddit(
 #     client_id="CLIENT_ID",
 #     client_secret="CLIENT_SECRET",
